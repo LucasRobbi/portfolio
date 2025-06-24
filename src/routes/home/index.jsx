7b1cb904bde71602029e131a02ac25cce2482045
Lucas Robbi <lucas.robbi.98@gmail.com>
2025-06-23 23:46:54 -0300

    refactor: seo e fonts

--- src/routes/home/index.jsx
+++ src/routes/home/index.jsx
@@ -1,9 +1,10 @@
 export default function Home() {
   return (
     <>
-      <section>
+      <section className="text-white">
         <div className="container">
           <h1 className="text-3xl font-bold underline">Home!</h1>
+          <p className="">teste texto</p>
         </div>
       </section>
     </>
